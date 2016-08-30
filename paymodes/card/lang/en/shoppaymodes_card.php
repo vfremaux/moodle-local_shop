1996798037d1e86bf2ab125a41d49c14575426dd
valery <valery.fremaux@gmail.com>
2016-08-30 22:47:37 +0200

    clean out non released plugins

--- paymodes/card/lang/en/shoppaymodes_card.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-$string['pluginname'] = 'Credit card payment (generic)';
-$string['enablecard'] = 'Credit card payment';
-$string['enablecard2'] = 'Credit card payment';
-$string['enablecard3'] = 'You choosed using credit card for payment...';
-$string['card'] = 'Credit card';
--- paymodes/card/lang/fr/shoppaymodes_card.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$string['pluginname'] = 'Réglement par carte bleue (générique)';
-$string['enablecard'] = 'Réglement par carte bleue';
-$string['enablecard2'] = 'Réglement par carte bleue';
-$string['enablecard3'] = 'Vous avez choisi de régler par carte bancaire à travers notre partenaire de paiment en ligne...';
-$string['card'] = 'Carte bleue';
-
