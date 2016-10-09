bf8c6adf2d77978c14d0b681d52d7d47e1e13840
valery <valery.fremaux@gmail.com>
2016-10-09 20:44:25 +0200

    fix js

--- front/js/order.js
+++ front/js/order.js
@@ -11,6 +11,19 @@ function send_confirm(){
     document.forms.bill.submit();
 }
 
+function haverequireddata() {
+    if (requiredorderfieldlist === null) {
+        return true;
+    }
+    for (i = 0; i < requiredorderfieldlist.length; i++) {
+        if (document.forms.bill.elements[requiredorderfieldlist[i]].value === '') {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 function listen_to_required_changes(){
     if (haverequireddata()){
         document.forms.confirmation.elements.go_confirm.disabled = false;
@@ -24,19 +37,6 @@ function listen_to_required_changes(){
 }
 
 var requiredorderfieldlist = null;
-
-function haverequireddata() {
-    if (requiredorderfieldlist === null) {
-        return true;
-    }
-    for (i = 0; i < requiredorderfieldlist.length; i++) {
-        if (document.forms.bill.elements[requiredorderfieldlist[i]].value === '') {
-            return false;
-        }
-    }
-
-    return true;
-}
 
 function accept_eulas(buttonobj){
     if (buttonobj.form.agreeeula.checked){
