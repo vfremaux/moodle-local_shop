825376a994f64a88f89af781cfd018099ca44889
valery <valery.fremaux@gmail.com>
2016-10-17 18:04:53 +0200

    fix validation

--- datahandling/handlers/std_setuponecoursesession/std_setuponecoursesession.class.php
+++ datahandling/handlers/std_setuponecoursesession/std_setuponecoursesession.class.php
@@ -316,7 +316,7 @@ class shop_handler_std_setuponecoursesession extends shop_handler {
      * Dismounts all effects of the handler production when a product is deleted.
      * The contexttype will denote the type of Moodle object that was created. some
      * hanlders may deal with several contexttypes if they have a complex production
-     * operation. the instanceid is moslty a moodle table id that points the concerned instance 
+     * operation. the instanceid is moslty a moodle table id that points the concerned instance
      * within the context type scope.
      *
      * In assignroleoncontext plugin, removes the role assignation
