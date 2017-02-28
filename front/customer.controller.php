e23a715446869d3f1112c6483c0e503b73ea9b7a
valery <valery.fremaux@gmail.com>
2017-02-28 15:14:41 +0100

    Restores customer duplicate email check

--- front/customer.controller.php
+++ front/customer.controller.php
@@ -117,7 +117,7 @@ class customer_controller extends front_controller_base {
                     shop_validate_invoicing();
                 }
 
-                if (empty($errors)) {
+                if (empty($shoppingcart->errors)) {
                     /*
                      * register customer in customer table now
                      * this allows us to catch customer list, even if not going through the whole purchase
