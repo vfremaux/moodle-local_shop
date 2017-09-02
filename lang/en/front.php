71174b1c8afbdfef8da1f3a79f5d1b5b4351c232
valery <valery.fremaux@gmail.com>
2017-09-02 19:33:07 +0200

    Automated sync from SVN

--- lang/en/front.php
+++ lang/en/front.php
@@ -243,19 +243,19 @@ $string['sales_feedback_tpl'] = '
 
 <p>You just ordered on the website : <%%SERVER%%>.</p>
 
-<p><u>Customer identification :</u> </p>
+<p><u>Customer identification:</u> </p>
 <hr />
 <b>Firstname :</b> <%%FIRSTNAME%%><br />
-<b>Lastname :</b> <%%LASTNAME%%><br />
+<b>Lastname:</b> <%%LASTNAME%%><br />
 <b>Mail :</b> <%%MAIL%%><br />
 <b>City :</b> <%%CITY%%><br />
 <b>Country :</b> <%%COUNTRY%%><br />
 <hr />
-<p><u>Order summary :</u></p>
+<p><u>Order summary:</u></p>
 <hr />
 <b>Total untaxed amount :</b> <%%AMOUNT%%><br />
-<b>Pay mode :</b> <%%PAYMODE%%><br />
-<b>Items :</b> <%%ITEMS%%>
+<b>Pay mode:</b> <%%PAYMODE%%><br />
+<b>Items:</b> <%%ITEMS%%>
 <hr />
 <%%PRODUCTION_DATA%%>
 <hr />Access to the online bill <a href=\"<%%SERVER_URL%%>/login/index.php?ticket=<%%TICKET%%>\">here</a>
